ac2cb0a8bc7521818781521049cc904dbbae5240
Luka Klacar <lukaklacar@gmail.com>
2018-03-31 22:09:35 +0200

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        axios
+            .post('http://localhost:8080/api/register', {login: 'asdasd', password: 'asdasdasdasdasd'})
+            .then(console.log)
+            .catch(console.log)
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/main/java/com/lukaklacar/example/config/WebConfigurer.java
+++ src/main/java/com/lukaklacar/example/config/WebConfigurer.java
@@ -1,5 +1,6 @@
 package com.lukaklacar.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,12 +10,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class WebConfigurer {
 
+    @Value("${application.cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin(allowedOrigin);
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.addExposedHeader("Authorization,Link,X-Total-Count");
